593da101fda65021ce50a788dbaff74043b7ceb9
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-04-16 21:40:57 +0300

    simple edit

--- lib/widgets/note_editor_screen.dart
+++ lib/widgets/note_editor_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:noteapp/style/app_style.dart';
